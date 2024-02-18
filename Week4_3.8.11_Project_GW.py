a15c092be443b29a92e5ca3286d11ddf88e1540a
Grakara <158855961+Grakara@users.noreply.github.com>
2024-02-18 12:56:01 -0700

    Add files via upload

---
+++ Week4_3.8.11_Project_GW.py
@@ -0,0 +1,20 @@
+import turtle
+wn = turtle.Screen()                    #Window
+wn.bgcolor("white")
+wn.title("Five Pointed Star")
+
+nein = turtle.Turtle()                  #Nein the turtle
+nein.color("black")
+nein.pensize(5)
+nein.shape("blank")
+nein.speed(15)
+
+nein.penup()
+nein.forward(-104.5)
+nein.pendown()
+
+for f in range(5):
+    nein.forward(209)
+    nein.right(144)
+                                        #five lines to draw the star
+
