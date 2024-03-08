454995da1ea2905908afe69b4fadee8865951aa9
Grakara <158855961+Grakara@users.noreply.github.com>
2024-03-07 17:57:49 -0700

    Add files via upload

---
+++ Week7-Project_6.9.7.py
@@ -0,0 +1,25 @@
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+def to_secs (hours, mins, secs):
+    """turns hours, minutes, and seconds into total seconds"""
+    return (hours*60*60)+(mins*60)+(secs)
+
+def test_suite():
+    """ Run the suite of tests for code in this module (this file).
+    """
+    test(to_secs(2, 30, 10) == 9010)
+    test(to_secs(2, 0, 0) == 7200)
+    test(to_secs(0, 2, 0) == 120)
+    test(to_secs(0, 0, 42) == 42)
+    test(to_secs(0, -10, 10) == -590)
+
+test_suite()
