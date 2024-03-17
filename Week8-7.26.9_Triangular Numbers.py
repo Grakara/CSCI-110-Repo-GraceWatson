0a5beacad161d124767703bc145ed583d947c961
Grakara <158855961+Grakara@users.noreply.github.com>
2024-03-17 15:18:25 -0600

    Update Week8-7.26.9_Triangular Numbers.py
    Added another function that creates the same results, just in a different way, and using the formula for triangular numbers from the homework video. I also added tests for all my functions.

--- Week8-7.26.9_Triangular Numbers.py
+++ Week8-7.26.9_Triangular Numbers.py
@@ -1,3 +1,14 @@
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
 def print_triangular_numbers(n):        #Using Code from textbook and making it my own
     """ Print all triangular numbers <=n """
     ss  = 0
@@ -20,6 +31,23 @@ def print_triangular_numbers2(n):       #Using for loop
     print()
     return n
 
+def print_triangular_numbers3(n):       #Using formula from internet adapted to work in python
+    """ Prints triangular numbers 1 through n """
+    count = 0
+    v = 0
+    t= 0
+    while v <= n-1:
+        count = count + 1
+        v = v + 1
+        t = int((v**2 +v)/2)
+        print(count, "\t", t)
+    print()
+    return t
 
 print_triangular_numbers(5)
 print_triangular_numbers2(5)
+print_triangular_numbers3(5)
+
+test(print_triangular_numbers(5) == 15)
+test(print_triangular_numbers2(5) == 15)
+test(print_triangular_numbers3(5) == 15)
