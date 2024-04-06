dd5ab9117a4bf3207501fac2a5a40451cabc7a5a
Grakara <158855961+Grakara@users.noreply.github.com>
2024-04-05 23:56:53 -0600

    Add files via upload

---
+++ Project 11.22.10.py
@@ -0,0 +1,26 @@
+import sys
+
+def test(did_pass):
+    """  Print the result of a test.  """
+    linenum = sys._getframe(1).f_lineno   # Get the caller's line number.
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+def replace(s, old, new):
+    """replaces all occurances of 'old' with 'new' in string 's'"""
+    remove_old = s.split(old)
+    NewStr = new.join(remove_old)
+    return (NewStr)
+
+            
+test(replace("Mississippi", "i", "I") == "MIssIssIppI")
+
+s = "I love spom! Spom is my favorite food. Spom, spom, yum!"
+test(replace(s, "om", "am") ==
+    "I love spam! Spam is my favorite food. Spam, spam, yum!")
+
+test(replace(s, "o", "a") ==
+    "I lave spam! Spam is my favarite faad. Spam, spam, yum!")
